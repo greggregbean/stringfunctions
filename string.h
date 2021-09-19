d68ac1afa9ab5fcd2b032df215eab326d3379fee
greggregbean <90098955+greggregbean@users.noreply.github.com>
2021-09-19 14:55:44 +0300

    Add files via upload

---
+++ string.h
@@ -0,0 +1,140 @@
+/// @file strings.h
+
+#include <stdio.h>
+#include <malloc.h>
+#include "doc.h"
+
+//----------------------------------------------
+// FUNCTIONS
+//----------------------------------------------
+
+char puts(char line[]);
+int strchar(char line[], char symbol);
+int strlen(char line[]);
+char* strcpy(char dst[], char src[]);
+char* strncpy(char dst[], char src[], int len);
+char* strcat(char dst[], char src[]);
+char* fgets(FILE *fp, char str[]);
+int getline(FILE *fp, int msxlen, char str[]);
+
+char puts(char line[])
+{
+    int lenOfLine = strlen(line);
+
+    for (int num = 0; num < lenOfLine; num++)
+    {
+        printf("%c", line[num]);
+    }
+
+    return '\n';
+}
+
+int strchar(char line[], char symbol)
+{
+    int num = 0;
+
+    int len = strlen(line);
+
+    for (num; num < len; num++)
+    {
+        if (line[num] == symbol)
+        {
+            printf("The %c has been mentioned first on %d position ", symbol, num);
+
+            return num;
+        }
+    }
+
+    printf("The %c has not been mentioned in this line ", symbol);
+
+    return NOTMENTIONED;
+}
+
+int strlen(char line[])
+{
+    int len = 0;
+
+    while (line[len] != '\0')
+    {
+        len++;
+    }
+
+    return len;
+}
+
+char* strcpy(char dst[], char src[])
+{
+    int len = strlen(src);
+
+    for (int num = 0; num < len; num++)
+    {
+        dst[num] = src [num];
+    }
+
+    return dst;
+}
+
+char* strncpy(char dst[], char src[], int len)
+{
+    for (int num = 0; num < len; num++)
+    {
+        dst[num] = src [num];
+    }
+
+    return dst;
+}
+
+char* strcat(char dst[], char src[])
+{
+    int lensrc = strlen(src);
+    int lendst = strlen(dst);
+
+    for(int i = 0; i < lensrc; i++)
+    {
+        dst[lendst] = src[i];
+        lendst++;
+    }
+
+    return dst;
+}
+
+char* strdup(char orig[])
+{
+    char *cpy = (char*) calloc(sizeof(orig), 1);
+
+    strcpy(cpy, orig);
+
+    return cpy;
+}
+
+char* strncat(char dst[], char src[], int num)
+{
+    int lensrc = strlen(src);
+    int lendst = strlen(dst);
+
+    for(int i = 0; i < num; i++)
+    {
+        dst[lendst] = src[i];
+        lendst++;
+    }
+
+    return dst;
+}
+
+char* fgets(FILE *fp, char str[])
+{
+    int i = 0;
+
+    while ((str[i] = getc(fp)) != EOF)
+    {
+        i++;
+    }
+
+    return str;
+}
+
+
+
+
+
+
---
+++ testing.cpp
@@ -0,0 +1,92 @@
+#include "string.h"
+
+int main()
+{
+
+//----------------------------------------------
+
+    printf("puts: \n");
+
+    char line[MAXLEN] = "1234abcdefg";
+    puts(line);
+
+    printf("\n\n");
+
+//----------------------------------------------
+
+    printf("strchar: \n");
+
+    strchar(line, 'a');
+
+    printf("\n\n");
+
+//----------------------------------------------
+
+    printf("strcpy: \n");
+
+    char src[MAXLEN] = "Hello world!";
+    char dst[MAXLEN] = {'\0'};
+    strcpy(dst, src);
+    puts(dst);
+
+    printf("\n\n");
+
+//----------------------------------------------
+
+    printf("strncpy: \n");
+
+    char src1[MAXLEN] = "Hello world!";
+    char dst1[MAXLEN] = {'\0'};
+    strncpy(dst1, src1, 3);
+    puts(dst1);
+
+    printf("\n\n");
+
+//----------------------------------------------
+
+    printf("strcat: \n");
+
+    char src2[MAXLEN] = "Hello world!";
+    char dst2[MAXLEN] = "USER ";
+    strcat(dst2, src2);
+    puts(dst2);
+
+    printf("\n\n");
+
+//----------------------------------------------
+
+    printf("strncat: \n");
+
+    char src3[MAXLEN] = "Hello world!";
+    char dst3[MAXLEN] = "USER ";
+    strncat(dst3, src3, 3);
+    puts(dst3);
+
+    printf("\n\n");
+
+//----------------------------------------------
+
+    printf("strdup: \n");
+
+    char src4[MAXLEN] = "HI";
+    char* str = strdup(src4);
+    puts(str);
+
+    printf("\n\n");
+
+//----------------------------------------------
+
+    printf("fgets: \n");
+
+    char str1[MAXLEN] = {'\0'};
+    FILE *fp;
+    char mytext[] = "mytext.txt";
+    fp = fopen(mytext, "r");
+    fgets(fp, str1);
+    puts(str1);
+
+    printf("\n\n");
+
+//----------------------------------------------
+
+}
